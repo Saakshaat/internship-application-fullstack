b26c64e6ddcbcd560d460ae12e56c2c8ea51bfe3
Saakshaat <saakshaat2001@gmail.com>
2020-04-14 17:42:45 -0400

    Saving returned variant in Cookie and returning variant in cookie

--- cf-workers/index.js
+++ cf-workers/index.js
@@ -1,17 +1,28 @@
 addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request));
+  if(event.request.method !== 'GET') return;
+  event.respondWith(handleRequest(event.request), );
 });
 
 async function handleRequest(request) {
   let urls = [];
-  let response = await fetch(`https://cfw-takehome.developers.workers.dev/api/variants`);
+  const endpoint = 'https://cfw-takehome.developers.workers.dev/api/variants';
+  let response = await fetch(endpoint);
   let variants = await response.json();
 
   urls = variants.variants;
 
-  var url = urls[Math.floor(Math.random() * urls.length)];  
+  const FIRST_VARIANT = new Response('first variant', fetch(urls[0]));
+  const SECOND_VARIANT = new Response('second variant', fetch(urls[1]));
 
-  return new Response(url, {
-    headers: { "content-type": "application/json" }
-  });
+  const cookie = request.headers.get('cookie')
+  if(cookie && cookie.includes('/1')) {
+    return FIRST_VARIANT;
+  } else if(cookie && cookie.includes('/2')) {
+    return SECOND_VARIANT;
+  } else {
+    let group = Math.random() < 0.5 ? 1 : 2;
+    let response = group === 2 ? SECOND_VARIANT : FIRST_VARIANT;
+    response.headers.append('Set-Cookie', `${endpoint}/${group}=${group}; path=/`)
+    return response;
+  }
 }
