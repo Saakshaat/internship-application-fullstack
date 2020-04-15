eb3ee9d953974eaa8a012fe11cd5c0f19f5b0408
Saakshaat <saakshaat2001@gmail.com>
2020-04-15 00:26:23 -0400

    HTML Rewrite to change HTMLs elements using the Cloudflare runtime API

--- cf-workers/index.js
+++ cf-workers/index.js
@@ -1,11 +1,11 @@
 addEventListener("fetch", event => {
-  if(event.request.method !== 'GET') return;
+  if (event.request.method !== "GET") return;
   event.respondWith(handleRequest(event.request));
 });
 
 async function handleRequest(request) {
-  const endpoint = 'https://cfw-takehome.developers.workers.dev/api/variants';
-  urls = await parse(endpoint);
+  const endpoint = "https://cfw-takehome.developers.workers.dev/api/variants";
+  const urls = await parse(endpoint);
 
   return await distributeRequests(request, endpoint, urls);
 }
@@ -18,26 +18,85 @@ async function parse(endpoint) {
 }
 
 async function distributeRequests(request, endpoint, urls) {
-  /** 
+  /**
    * Following A/B Testing methodology for equally distibuting the requests between the 2 variants
    * Saving the returned variant in a cookie for Persisting Variants
-  */
- const FIRST_VARIANT = new Response('first variant', await fetch(urls[0]));
- const SECOND_VARIANT = new Response('second variant', await fetch(urls[1]));
- 
- return processCookie(request, endpoint, FIRST_VARIANT, SECOND_VARIANT);
+   */
+
+  const firstVariant = await fetch(urls[0]);
+  const firstContent = await rewriteHTML('First', await firstVariant).text();
+  const secondVariant = await fetch(urls[1]);
+  const secondContent = await rewriteHTML('Second', await secondVariant).text();
+
+  const FIRST_RESPONSE = new Response(firstContent);
+  const SECOND_RESPONSE = new Response(secondContent);
+
+  return processCookie(
+    request,
+    endpoint,
+    FIRST_RESPONSE,
+    SECOND_RESPONSE,
+    urls
+  );
 }
 
-const processCookie = (request, endpoint, first, second) => {
-  const cookie = request.headers.get('cookie')
-  if(cookie && cookie.includes('/1')) {
-      return first
-    } else if(cookie && cookie.includes('/2')) {
-      return second
-    } else {
-      let group = Math.random() < 0.5 ? 1 : 2;
-      let response = group === 2 ? first : second;
-      response.headers.append('Set-Cookie', `${endpoint}/${group}=${group}; path=/`);
-      return response;
-    }
+function rewriteHTML(variant, content) {
+  return new HTMLRewriter()
+      .on('title', {
+        element(element) {
+          element.setInnerContent(`${variant} Variant, Saakshaat`);
+        }
+      })
+      .on('a#url', new AttributeHandler('href'))
+      .on('a#url', {
+        element(element) {
+          element.setInnerContent('Check out this awesome developer')
+        }
+      })
+      .on('p#description', {
+        element(element) {
+          element.setInnerContent(`I will confess, the HTMLRewriter API was impressive. <br> Here's the ${variant} page.`)
+        }
+      })
+      .transform(content);
 }
+
+class AttributeHandler {
+  constructor(attributeName){
+    this.attributeName = attributeName;
+  }
+
+  element(element) {
+    console.log("I'm here");
+    const attribute = element.getAttribute(this.attributeName)
+    if(attribute) {
+      element.setAttribute(
+        this.attributeName,
+        attribute.replace('https://cloudflare.com', 'https://saakshaat.github.io')
+      )
+    }
+  }
+}
+
+
+const processCookie = (request, endpoint, first, second, urls) => {
+  const cookie = request.headers.get("cookie");
+  let response;
+
+  if (cookie && cookie.includes("/1")) {
+    response = first;
+  } else if (cookie && cookie.includes("/2")) {
+    response = second;
+  } else {
+    let group = Math.random() < 0.5 ? 1 : 2;
+    response = group === 2 ? first : second;
+    response.headers.append(
+      "Set-Cookie",
+      `${endpoint}/${group}=${group}; path=/`
+    );
+    req = new Request(`${endpoint}/${group}`);
+    console.log(req);
+    response;
+  }
+  return response;
+};
